8ccd6a2f019bd372e70e8c26af52f1e9668e0e59
shamank <russyakrusya@gmail.com>
2022-03-26 22:55:49 +0300

    add debug toolbar

--- shorturl/shorturl/urls.py
+++ shorturl/shorturl/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 
 
@@ -24,3 +26,9 @@ urlpatterns = [
     path('user/', include('users.urls')),
     path('captcha/', include('captcha.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
